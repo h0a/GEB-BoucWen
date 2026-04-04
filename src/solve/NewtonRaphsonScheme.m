189e7d099e68a0dfa3965ec7360f84927eb0bf3c
Hoa Nguyen <nguyenhoa18121993@gmail.com>
2026-04-04 10:13:55 +0200

    move the newton raphson to a separated function

---
+++ src/solve/NewtonRaphsonScheme.m
@@ -0,0 +1,28 @@
+function [x,beam,mesh] = NewtonRaphsonScheme(beam,mesh,x,loadFac,loadStep)
+
+alpha = loadFac;
+i = loadStep;
+
+NRiter = 0;                                 % iteration count
+
+while NRiter < beam.maxNumIter              % LOOP NR AT CURRENT LOAD STEP
+    NRiter = NRiter + 1;
+
+    [x, deltax] = solveLinSys(beam,mesh,alpha,x);
+
+    if norm(deltax) <= beam.NRtol
+        break
+    end
+end                                         % END LOOP NR AT CURRENT LOAD STEP
+
+% storing NR-converged solutions at the current load step
+mesh.num_iters(i)   = NRiter;
+beam.q(:,i+1)       = x(1:mesh.ndofs);
+beam.e(:,i+1)       = x(mesh.ndofs+1:mesh.ndofs+mesh.ndofse);
+beam.s(:,i+1)       = x(mesh.ndofs+mesh.ndofse+1:mesh.ndofs+mesh.ndofse+mesh.ndofss);
+beam.chi(:,i+1)     = x(mesh.ndofs+mesh.ndofse+mesh.ndofss+1:end);
+
+% stopping computation in case of non-converging NR solution at the current load step
+if NRiter == beam.maxNumIter
+    warning('Newton-Raphson scheme does not converge at load step %.1d.\nComputation stopped.\n', i);
+end
--- src/solve/solveStaticGEB.m
+++ src/solve/solveStaticGEB.m
@@ -35,29 +35,13 @@ x = [beam.q(:,1); beam.e(:,1); beam.s(:,1); beam.chi(:,1)];
 
 % loop over load steps
 for i = 1:beam.numLoadSteps                     % LOOP OVER LOAD STEPS
-    alpha = beam.loadFactors(i);                % load factor    
-    NRiter = 0;                                 % iteration count
-        
-    while NRiter < beam.maxNumIter              % LOOP NR AT CURRENT LOAD STEP
-        NRiter = NRiter + 1;
-
-        [x, deltax] = solveLinSys(beam,mesh,alpha,x);
-
-        if norm(deltax) <= beam.NRtol
-            break
-        end
-    end                                         % END LOOP NR AT CURRENT LOAD STEP
-
-    % storing NR-converged solutions at the current load step
-    mesh.num_iters(i)   = NRiter;
-    beam.q(:,i+1)       = x(1:mesh.ndofs);
-    beam.e(:,i+1)       = x(mesh.ndofs+1:mesh.ndofs+mesh.ndofse);
-    beam.s(:,i+1)       = x(mesh.ndofs+mesh.ndofse+1:mesh.ndofs+mesh.ndofse+mesh.ndofss);
-    beam.chi(:,i+1)     = x(mesh.ndofs+mesh.ndofse+mesh.ndofss+1:end);
+    alpha = beam.loadFactors(i);                % load factor 
+    
+    % solving with Newton Raphson
+    [x,beam,mesh] = NewtonRaphsonScheme(beam,mesh,x,alpha,i);
     
     % stopping computation in case of non-converging NR solution at the current load step
-    if NRiter == beam.maxNumIter
-        warning('Newton-Raphson scheme does not converge at load step %.1d.\nComputation stopped.\n', i);
+    if mesh.num_iters(i) == beam.maxNumIter
         break;
     end
 end                                             % END LOOP OVER LOAD STEPS
